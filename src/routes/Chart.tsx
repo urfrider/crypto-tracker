6adb6ad7936d4d2576f19d6bd5c6d51dbbcc0d48
wnsl9704 <wnsl9704@naver.com>
2022-09-30 11:46:35 +0800

    updated charts

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,19 +1,30 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
     coinId: string;
 }
 
-interface ChartData {
 
+interface ChartData {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
 }
 
 function Chart({coinId}:ChartProps) {
-    const {isLoading, data} = useQuery(["chart", coinId], () => fetchCoinHistory(coinId));
+    const {isLoading, data} = useQuery<ChartData>(["chart", coinId], () => fetchCoinHistory(coinId));
     console.log(fetchCoinHistory(coinId));
     return (
-        <h1>Chart</h1>
+        <>
+            {isLoading? <h3>Chart Loading...</h3> : <ApexChart />}
+        </>
     )
 }
 
